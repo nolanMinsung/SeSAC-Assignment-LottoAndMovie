026c7642ca9e3efa7b08dc3fc86e93fe4503ce05
Minsung Kim <mskim4048@naver.com>
2025-07-23 21:33:56 +0900

    Initial Commit

---
+++ Assignment-LottoAndMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment-LottoAndMovie
+//
+//  Created by 김민성 on 7/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

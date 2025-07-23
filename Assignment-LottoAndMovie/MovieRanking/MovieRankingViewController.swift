0b049614331851133ad578c2f44c14bcb59e7c0a
Minsung Kim <mskim4048@naver.com>
2025-07-23 21:48:44 +0900

    add: 로또 및 영화 순위 뷰컨트롤러 추가

---
+++ Assignment-LottoAndMovie/MovieRanking/MovieRankingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MovieRankingViewController.swift
+//  Assignment-LottoAndMovie
+//
+//  Created by 김민성 on 7/23/25.
+//
+
+import UIKit
+
+class MovieRankingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

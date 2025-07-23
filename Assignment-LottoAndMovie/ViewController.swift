fb1caa60d3eda6cdd066db432078ad6b8202a669
Minsung Kim <mskim4048@naver.com>
2025-07-23 21:47:28 +0900

    feat: ViewController UI 구현

--- Assignment-LottoAndMovie/ViewController.swift
+++ Assignment-LottoAndMovie/ViewController.swift
@@ -7,17 +7,36 @@
 
 import UIKit
 
+import SnapKit
+
 class ViewController: UIViewController {
     
-    let lottoButton = UIButton()
-    let moviewButton = UIButton()
+    let lottoButton = UIButton(configuration: .tinted())
+    let moviewButton = UIButton(configuration: .tinted())
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        lottoButton.configuration?.image = UIImage(systemName: "dollarsign.bank.building")
+        lottoButton.configuration?.title = "로또"
+        lottoButton.configuration?.subtitle = "일확천금을 노리세요?"
+        lottoButton.configuration?.titlePadding = 10
+        
+        moviewButton.configuration?.image = UIImage(systemName: "movieclapper")
+        moviewButton.configuration?.title = "영화"
+        moviewButton.configuration?.subtitle = "영화 볼 시간이 있어요?"
+        moviewButton.configuration?.titlePadding = 10
+        
         view.addSubview(lottoButton)
         view.addSubview(moviewButton)
-        
+        lottoButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(200)
+        }
+        moviewButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(lottoButton.snp.bottom).offset(100)
+        }
     }
     
     
